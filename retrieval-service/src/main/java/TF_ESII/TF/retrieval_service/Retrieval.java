c43b9f3d0356ced7225d9e322a3ac4b7522061a3
LeoPasa <pasa.leonardo@hotmail.com>
2026-06-27 17:56:48 -0300

    correções de estrutura e sintaxe mais uma vez (falta dependência de starter web)

--- retrieval-service/src/main/java/TF_ESII/TF/retrieval_service/Retrieval.java
+++ retrieval-service/src/main/java/TF_ESII/TF/retrieval_service/Retrieval.java
@@ -1,5 +1,6 @@
 package TF_ESII.TF.retrieval_service;
 
+
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.ai.vectorstore.SearchRequest;
@@ -7,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Map;
-import javax.naming.directory.SearchResult;
 
 @Service
 public class Retrieval {
--- retrieval-service/src/main/java/TF_ESII/TF/retrieval_service/RetrievalController.java
+++ retrieval-service/src/main/java/TF_ESII/TF/retrieval_service/RetrievalController.java
@@ -1,13 +1,19 @@
 package TF_ESII.TF.retrieval_service;
 
 import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.ai.document.Document;
+import java.util.Map;
+
+
+
 
 @RestController
 @RequestMapping("/retrieval")
@@ -22,7 +28,7 @@ public class RetrievalController {
 	}
 
 	@GetMapping("/search")
-	public List<Documento> pesquisar(@RequestParam String pergunta){
+	public List<Document> pesquisar(@RequestParam String pergunta){
 		return retrieval.buscarDocumentos(pergunta);
 	}
     
